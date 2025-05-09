482bc285a86932472fa94854ae2afad94531bfd8
kce0508 <kce0508@gmail.com>
2025-05-09 17:21:02 +0900

    2일차

---
+++ day02/Day02Wpf/WpfBasicApp02/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using WpfBasicApp02.ViewModels;
+
+namespace WpfBasicApp02
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper() { 
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<MainViewModel>();
+        }
+    }
+}
